d6fd68935c6039b68e312b149d47cf211e795720
rathlucas <lucin189@gmail.com>
2022-06-19 22:15:44 -0300

    Create mock api server with miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { createServer } from 'miragejs';
+
 import { App } from './App';
 import { GlobalStyle } from './styles/Global';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
+
 root.render(
   <React.StrictMode>
     <GlobalStyle />
